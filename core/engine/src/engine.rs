c9b5b9cf0584bb775ea3210183c6b3152fe9d77e
Pmyl <julo134@gmail.com>
2026-05-28 10:45:37 +0100

    Another

--- core/engine/src/engine.rs
+++ core/engine/src/engine.rs
@@ -22,3 +22,60 @@ pub fn read(file_bytes: &[u8], from_byte: u32, length: u32) -> Vec<FileRow> {
 
     result
 }
+
+#[derive(Serialize)]
+pub struct Details {
+    binary: String,
+    be_decimal_8: String,
+    le_decimal_8: String,
+    be_decimal_16: Option<String>,
+    le_decimal_16: Option<String>,
+    be_decimal_32: Option<String>,
+    le_decimal_32: Option<String>,
+    be_decimal_64: Option<String>,
+    le_decimal_64: Option<String>,
+    be_decimal_128: Option<String>,
+    le_decimal_128: Option<String>,
+}
+
+pub fn details(file_bytes: &[u8], from_byte: u32) -> Details {
+    let current_byte = file_bytes[from_byte as usize];
+    let u8_details = (
+        u8::from_ne_bytes([current_byte]),
+        u8::from_le_bytes([current_byte]),
+    );
+
+    let u16_details = file_bytes
+        .get(from_byte as usize..(from_byte + 2) as usize)
+        .and_then(|b| b.try_into().ok())
+        .map(|arr| (u16::from_be_bytes(arr), u16::from_le_bytes(arr)));
+
+    let u32_details = file_bytes
+        .get(from_byte as usize..(from_byte + 4) as usize)
+        .and_then(|b| b.try_into().ok())
+        .map(|arr| (u32::from_be_bytes(arr), u32::from_le_bytes(arr)));
+
+    let u64_details = file_bytes
+        .get(from_byte as usize..(from_byte + 8) as usize)
+        .and_then(|b| b.try_into().ok())
+        .map(|arr| (u64::from_be_bytes(arr), u64::from_le_bytes(arr)));
+
+    let u128_details = file_bytes
+        .get(from_byte as usize..(from_byte + 16) as usize)
+        .and_then(|b| b.try_into().ok())
+        .map(|arr| (u128::from_be_bytes(arr), u128::from_le_bytes(arr)));
+
+    Details {
+        binary: format!("{:08b}", current_byte),
+        be_decimal_8: format!("{}", u8_details.0).to_string(),
+        le_decimal_8: format!("{}", u8_details.1).to_string(),
+        be_decimal_16: u16_details.map(|x| x.0).map(|x| format!("{}", x)),
+        le_decimal_16: u16_details.map(|x| x.1).map(|x| format!("{}", x)),
+        be_decimal_32: u32_details.map(|x| x.0).map(|x| format!("{}", x)),
+        le_decimal_32: u32_details.map(|x| x.1).map(|x| format!("{}", x)),
+        be_decimal_64: u64_details.map(|x| x.0).map(|x| format!("{}", x)),
+        le_decimal_64: u64_details.map(|x| x.1).map(|x| format!("{}", x)),
+        be_decimal_128: u128_details.map(|x| x.0).map(|x| format!("{}", x)),
+        le_decimal_128: u128_details.map(|x| x.1).map(|x| format!("{}", x)),
+    }
+}
--- core/wasm-engine/src/wasm-engine.rs
+++ core/wasm-engine/src/wasm-engine.rs
@@ -1,4 +1,3 @@
-use serde::Serialize;
 use wasm_bindgen::prelude::*;
 
 #[wasm_bindgen]
@@ -13,63 +12,8 @@ pub fn read(file_bytes: &[u8], from_byte: u32, length: u32) -> Result<JsValue, J
         .map_err(|e| JsError::new(format!("cannot parse result: {}", e).as_str()))
 }
 
-#[derive(Serialize)]
-pub struct Details {
-    binary: String,
-    be_decimal_8: String,
-    le_decimal_8: String,
-    be_decimal_16: Option<String>,
-    le_decimal_16: Option<String>,
-    be_decimal_32: Option<String>,
-    le_decimal_32: Option<String>,
-    be_decimal_64: Option<String>,
-    le_decimal_64: Option<String>,
-    be_decimal_128: Option<String>,
-    le_decimal_128: Option<String>,
-}
-
 #[wasm_bindgen]
 pub fn details(file_bytes: &[u8], from_byte: u32) -> Result<JsValue, JsError> {
-    let current_byte = file_bytes[from_byte as usize];
-    let u8_details = (
-        u8::from_ne_bytes([current_byte]),
-        u8::from_le_bytes([current_byte]),
-    );
-
-    let u16_details = file_bytes
-        .get(from_byte as usize..(from_byte + 2) as usize)
-        .and_then(|b| b.try_into().ok())
-        .map(|arr| (u16::from_be_bytes(arr), u16::from_le_bytes(arr)));
-
-    let u32_details = file_bytes
-        .get(from_byte as usize..(from_byte + 4) as usize)
-        .and_then(|b| b.try_into().ok())
-        .map(|arr| (u32::from_be_bytes(arr), u32::from_le_bytes(arr)));
-
-    let u64_details = file_bytes
-        .get(from_byte as usize..(from_byte + 8) as usize)
-        .and_then(|b| b.try_into().ok())
-        .map(|arr| (u64::from_be_bytes(arr), u64::from_le_bytes(arr)));
-
-    let u128_details = file_bytes
-        .get(from_byte as usize..(from_byte + 16) as usize)
-        .and_then(|b| b.try_into().ok())
-        .map(|arr| (u128::from_be_bytes(arr), u128::from_le_bytes(arr)));
-
-    let result = Details {
-        binary: format!("{:08b}", current_byte),
-        be_decimal_8: format!("{}", u8_details.0).to_string(),
-        le_decimal_8: format!("{}", u8_details.1).to_string(),
-        be_decimal_16: u16_details.map(|x| x.0).map(|x| format!("{}", x)),
-        le_decimal_16: u16_details.map(|x| x.1).map(|x| format!("{}", x)),
-        be_decimal_32: u32_details.map(|x| x.0).map(|x| format!("{}", x)),
-        le_decimal_32: u32_details.map(|x| x.1).map(|x| format!("{}", x)),
-        be_decimal_64: u64_details.map(|x| x.0).map(|x| format!("{}", x)),
-        le_decimal_64: u64_details.map(|x| x.1).map(|x| format!("{}", x)),
-        be_decimal_128: u128_details.map(|x| x.0).map(|x| format!("{}", x)),
-        le_decimal_128: u128_details.map(|x| x.1).map(|x| format!("{}", x)),
-    };
-
-    serde_wasm_bindgen::to_value(&result)
+    serde_wasm_bindgen::to_value(&engine::details(file_bytes, from_byte))
         .map_err(|e| JsError::new(format!("cannot parse result: {}", e).as_str()))
 }
