58c57e5322bbb53e2e39349720de70d734696d05
Pmyl <julo134@gmail.com>
2026-06-03 11:52:32 +0100

    ratatui: Properly show text in search bar when longer, also show cursor

--- view-ratatui/src/ui/search_bar.rs
+++ view-ratatui/src/ui/search_bar.rs
@@ -1,8 +1,9 @@
 use ratatui::{
     Frame,
-    layout::{Constraint, Layout, Rect},
+    layout::{Constraint, Layout, Position, Rect},
     style::{Color, Style},
-    widgets::{Block, Clear, Paragraph},
+    text::Span,
+    widgets::{Block, Clear},
 };
 
 use crate::state::{AppState, Focus};
@@ -28,11 +29,64 @@ pub fn render(frame: &mut Frame, area: Rect, state: &AppState) {
 
     frame.render_widget(Clear, middle);
 
-    let content = Paragraph::new(state.search_term.as_str()).block(
-        Block::bordered()
-            .title(" Search ")
-            .border_style(Style::new().fg(Color::Cyan)),
-    );
+    let block = Block::bordered()
+        .title(" Search ")
+        .border_style(Style::new().fg(Color::Cyan));
+    let inner = block.inner(middle);
+    frame.render_widget(block, middle);
 
-    frame.render_widget(content, middle);
+    let search_box_size = inner.width as u32;
+    let cursor_position = state.search_cursor_position % search_box_size;
+    let scroll_amount = state.search_cursor_position / search_box_size;
+
+    frame
+        .buffer_mut()
+        .cell_mut(Position {
+            x: inner.x + cursor_position as u16,
+            y: inner.y,
+        })
+        .map(|c| c.set_bg(Color::White));
+
+    let search_term_to_show = &state.search_term[(scroll_amount * search_box_size) as usize..];
+    let content = Span::from(search_term_to_show);
+    frame.render_widget(content, inner);
+
+    show_icon_for_more_text_before(frame, inner, scroll_amount);
+    show_icon_for_more_text_after(
+        frame,
+        inner,
+        scroll_amount,
+        search_box_size,
+        &state.search_term,
+    );
+}
+
+fn show_icon_for_more_text_before(frame: &mut Frame, area: Rect, scroll_amount: u32) {
+    if scroll_amount > 0 {
+        frame
+            .buffer_mut()
+            .cell_mut(Position {
+                x: area.x - 1,
+                y: area.y,
+            })
+            .map(|c| c.set_char('<'));
+    }
+}
+
+fn show_icon_for_more_text_after(
+    frame: &mut Frame,
+    area: Rect,
+    scroll_amount: u32,
+    search_box_size: u32,
+    search_term: &str,
+) {
+    if ((scroll_amount * search_box_size + search_box_size) as usize) < search_term.len() {
+        frame
+            .buffer_mut()
+            .cell_mut(Position {
+                x: area.x + area.width,
+                y: area.y,
+            })
+            .map(|c| c.set_char('>'));
+    }
 }
